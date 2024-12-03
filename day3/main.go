b7233b6eeaf6139c38e3107c3fa7e1d87f2fcc55
Joris <github@boekelcity.nl>
2024-12-03 13:37:05 +0100

    feat: Day 3 part 2

--- day3/main.go
+++ day3/main.go
@@ -12,16 +12,29 @@ func main() {
 	if err != nil {
 		fmt.Print(err)
 	}
-	r := regexp.MustCompile(`mul\((\d{1,3}),(\d{1,3})\)`)
+	r := regexp.MustCompile(`((mul|do|don't)\((\d{1,3})?,?(\d{1,3})?\))`)
 	matches := r.FindAllStringSubmatch(string(file), -1)
 
 	part1 := 0
+	part2 := 0
+	enabled := true
 	for _, m := range matches {
-		a, _ := strconv.Atoi(m[1])
-		b, _ := strconv.Atoi(m[2])
-		part1 += a * b
+		instruction, m1, m2 := m[2], m[3], m[4]
+		switch instruction {
+		case "mul":
+			a, _ := strconv.Atoi(m1)
+			b, _ := strconv.Atoi(m2)
+			part1 += a * b
+			if enabled {
+				part2 += a * b
+			}
+		case "do":
+			enabled = true
+		case "don't":
+			enabled = false
+		}
 	}
 
 	fmt.Println("Part 1:", part1)
-	//fmt.Println("Part 2:", part2)
+	fmt.Println("Part 2:", part2)
 }
