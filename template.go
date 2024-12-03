c728307eeec2b740c76bd3c1e816ee1c451c6a6c
Joris <github@boekelcity.nl>
2024-12-03 12:50:15 +0100

    feat: add part printing to template

--- template.go
+++ template.go
@@ -16,10 +16,16 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 
+	part1 := 0
+	part2 := 0
+
 	for scanner.Scan() {
 		t := scanner.Text()
 		// Todo: run day
 		fmt.Println(t)
 
 	}
+
+	fmt.Println("Part 1:", part1)
+	fmt.Println("Part 2:", part2)
 }
