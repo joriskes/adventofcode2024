cbfeb464134f7d42f9489863e1e85c5e952d061e
Joris <github@boekelcity.nl>
2024-12-07 13:41:32 +0100

    feat: Day 6 (fast)

--- day6/main.go
+++ day6/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"slices"
 	"strconv"
 	"strings"
 	"time"
@@ -41,29 +42,20 @@ func printLayout(layout map[int]map[int]Tile) {
 		}
 		fmt.Println(toPrint)
 	}
-
 }
 
-// We don't want to deep copy every bruteforce attempt so the id serves as a check if the visitdirection belongs to this attempt or a past one
-func runLayout(layout map[int]map[int]Tile, guard Guard, part1 bool, id string) (int, bool) {
+func runLayoutPart1(layout map[int]map[int]Tile, guard Guard) int {
 	res := 1 // First location is also a step
 	steps := 0
 	for guard.inBounds {
 		steps++
-		// Detect loop
-		if layout[guard.y][guard.x].visited && layout[guard.y][guard.x].visitId == id && layout[guard.y][guard.x].visitDirection == guard.direction {
-			return 0, true
-		}
 		// This is ugly
 		layout[guard.y][guard.x] = Tile{
-			obstacle:       false,
-			visited:        true, // Mark current location visited
-			visitDirection: guard.direction,
-			visitId:        id,
+			obstacle: false,
+			visited:  true, // Mark current location visited
 		}
 		nextX := guard.x
 		nextY := guard.y
-
 		switch guard.direction {
 		case 0:
 			nextY--
@@ -80,20 +72,73 @@ func runLayout(layout map[int]map[int]Tile, guard Guard, part1 bool, id string) 
 			if layout[nextY][nextX].obstacle {
 				guard.direction = (guard.direction + 1) % 4
 			} else {
-				if part1 && !layout[nextY][nextX].visited {
+				if !layout[nextY][nextX].visited {
 					res++
 				}
 				guard.x = nextX
 				guard.y = nextY
 			}
 		}
+	}
+	return res
+}
+
+// We don't want to deep copy every bruteforce attempt so the id serves as a check if the visitdirection belongs to this attempt or a past one
+func runLayoutPart2(layout map[int]map[int]Tile, guard Guard, id string) bool {
+	steps := 0
+	for guard.inBounds {
+		steps++
+		// Detect loop
+		if layout[guard.y][guard.x].visited && layout[guard.y][guard.x].visitId == id && layout[guard.y][guard.x].visitDirection == guard.direction {
+			return true
+		}
+		// This is ugly
+		layout[guard.y][guard.x] = Tile{
+			obstacle:       false,
+			visited:        true, // Mark current location visited
+			visitDirection: guard.direction,
+			visitId:        id,
+		}
+		nextX := guard.x
+		nextY := guard.y
+
+		// Find next obstacle in direction the guard is going
+		switch guard.direction {
+		case 0:
+			for !layout[nextY-1][nextX].obstacle && nextY >= 0 {
+				steps++
+				nextY--
+			}
+		case 1:
+			for !layout[nextY][nextX+1].obstacle && nextX < len(layout[nextY]) {
+				steps++
+				nextX++
+			}
+		case 2:
+			for !layout[nextY+1][nextX].obstacle && nextY < len(layout) {
+				steps++
+				nextY++
+			}
+		case 3:
+			for !layout[nextY][nextX-1].obstacle && nextX >= 0 {
+				steps++
+				nextX--
+			}
+		}
+		if nextY < 0 || nextY >= len(layout) || nextX < 0 || nextX >= len(layout[nextY]) {
+			guard.inBounds = false
+		} else {
+			guard.direction = (guard.direction + 1) % 4
+			guard.x = nextX
+			guard.y = nextY
+		}
 
 		if steps > 10000 {
-			return 0, true
+			return true
 		}
 	}
 
-	return res, false
+	return false
 }
 
 func main() {
@@ -134,19 +179,28 @@ func main() {
 		y++
 	}
 
-	part1, _ = runLayout(layout, guard, true, "p1")
+	part1 = runLayoutPart1(layout, guard)
+
+	// Only place obstacles on visited spots
+	todoList := []int{}
+	for y := 0; y < len(layout); y++ {
+		for x := 0; x < len(layout[y]); x++ {
+			if layout[y][x].visited {
+				todoList = append(todoList, y*len(layout)+x)
+			}
+		}
+	}
 
 	for y := 0; y < len(layout); y++ {
 		for x := 0; x < len(layout[y]); x++ {
-			if !layout[y][x].obstacle {
+			if slices.Contains(todoList, y*len(layout)+x) && !layout[y][x].obstacle {
 				// Place obstacle
 				layout[y][x] = Tile{
 					obstacle: true,
 					visited:  false,
 				}
 				// Test run if loop
-				_, loop := runLayout(layout, guard, false, strconv.Itoa(x)+"-"+strconv.Itoa(y))
-				if loop {
+				if runLayoutPart2(layout, guard, strconv.Itoa(x)+"-"+strconv.Itoa(y)) {
 					part2++
 				}
 				// Reset layout and guard
